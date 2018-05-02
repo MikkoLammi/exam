16e2c107117df0b51219aab37bb0b49282481411
Valtteri Rouvinen <valtteri.rouvinen@arcusys.fi>
2018-05-02 09:46:34 +0300

    CSCEXAM-0000 Fixed karma test.

--- test/unit/administrative/statistics/statisticsControllerSpec.js
+++ test/unit/administrative/statistics/statisticsControllerSpec.js
@@ -43,11 +43,10 @@ describe('StatisticsController', function () {
     });
 
     it('should have load participation statistics', function () {
-        $httpBackend.expectGET('/app/reports/participations?end=Tue+Mar+01+2016+00:00:00+GMT%2B0200&start=Tue+Dec+01+2015+00:00:00+GMT%2B0200')
+        $httpBackend.expectGET(/\/app\/reports\/participations\?end=[\w:+^&]/)
             .respond(readFixtures('participations.json'));
-        scope.dateService.startDate = 'Tue Dec 01 2015 00:00:00 GMT+0200';
-        scope.dateService.endDate = 'Tue Mar 01 2016 00:00:00 GMT+0200';
-        scope.listParticipations();
+        ctrl.endDate = 'Tue Mar 01 2016 12:00:00 GMT';
+        ctrl.listParticipations();
         $httpBackend.flush();
 
         // Check participations
@@ -56,10 +55,10 @@ describe('StatisticsController', function () {
         expect(Object.keys(scope.participations).length > 0).toBeTruthy();
 
         // Check min and max date
-        console.info('Min date: ' + new Date(scope.minDate));
-        expect(scope.minDate).toEqual(1449493200134);
-        console.info('Max date: ' + new Date(scope.maxDate));
-        expect(scope.maxDate).toEqual(1456783200000);
+        console.info('Min date: ' + new Date(ctrl.minDate));
+        expect(ctrl.minDate).toEqual(1449493200134);
+        console.info('Max date: ' + new Date(ctrl.maxDate));
+        expect(ctrl.maxDate).toEqual(1456833600000);
 
         // Check months
         expect(scope.months.length).toEqual(4);
